5afe2cae995325a425bcaa7da32ef82560fe76a6
sanginjeong <sanginjeong07@gmail.com>
2025-08-13 05:33:20 +0900

    ✨ Feat: Layout 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route element={/* 레이아웃 */ null}>
+        <Route element={<Layout />}>
           <Route path="/" element={/* 메인페이지 */ null} />
           <Route path="/list" element={/* 롤링페이퍼 목록페이지 */ null} />
+
           <Route path="/post">
             <Route index element={/* 롤링페이퍼 생성페이지 */ null} />
-            <Route path=":id" element={/* 생성된 롤링페이퍼 페이지 */ null}>
-              <Route path="edit" element={/* 롤링페이퍼 수정 페이지 */ null} />
-              <Route path="message" element={/* 롤링페이퍼 메세지 보내기 페이지 */ null} />
+            <Route path=":id/message" element={/* 롤링페이퍼 메세지 보내기 페이지 */ null} />
+            <Route element={<div>헤더서비스입니다</div>}>
+              <Route path=":id" element={/* 생성된 롤링페이퍼 페이지 */ null} />
+              <Route path=":id/edit" element={/* 롤링페이퍼 수정 페이지 */ null} />
             </Route>
           </Route>
         </Route>
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+const Layout = () => {
+  return (
+    <>
+      <header>헤더입니다!!!</header>
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
