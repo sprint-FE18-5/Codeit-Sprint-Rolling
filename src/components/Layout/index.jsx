4002bb1af5207bb5b1be9d703e8823810a2f62dd
sanginjeong <sanginjeong07@gmail.com>
2025-08-18 03:35:18 +0900

    ✨ Feat: Layout에서 데이터 받아서 헤더서비스와 outlet에 넘겨주기

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,18 +1,34 @@
-import { Outlet } from "react-router";
+import { Outlet, useParams } from "react-router";
 import { Header, HeaderService } from "../Header";
 import { VISIBLE_HEADER_SERVICE_PATHS } from "../../constants/VISIBLE_PATHS";
 import useIsMatchURL from "../../hooks/useIsMatchURL";
+import getRecipients from "../../api/getRecipients";
+import { useEffect, useState } from "react";
 
 const Layout = () => {
-  const isMatchURL = useIsMatchURL(VISIBLE_HEADER_SERVICE_PATHS);
+  const params = useParams();
+  const { id: recipientId } = params;
+  const isMatchURL = useIsMatchURL(VISIBLE_HEADER_SERVICE_PATHS); // ["/post/:id", "/post/:id/edit"]
+  const [recipient, setRecipient] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await getRecipients({ recipientId });
+      setRecipient(response);
+    };
+
+    if (isMatchURL) {
+      getData();
+    }
+  }, []);
 
   return (
     <>
       <header className="fixed left-0 right-0 top-0 z-99">
         <Header />
-        {isMatchURL && <HeaderService />}
+        {isMatchURL && <HeaderService recipient={recipient} />}
       </header>
-      <Outlet />
+      <Outlet context={{ recipient }} />
     </>
   );
 };
