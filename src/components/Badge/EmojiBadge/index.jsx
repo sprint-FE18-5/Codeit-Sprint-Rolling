85df6e9fead64701eca693da17df40609e90b6e1
sanginjeong <sanginjeong07@gmail.com>
2025-08-17 14:37:59 +0900

    ⚡️ Refactor: 이모지 뱃지 시맨틱 태그 변겅 및 스타일 개선

--- src/components/Badge/EmojiBadge/index.jsx
+++ src/components/Badge/EmojiBadge/index.jsx
@@ -1,10 +1,8 @@
 const EmojiBadge = ({ reaction }) => {
   return (
-    <div className="rounded-[32px] w-[66px] px-[12px] py-[8px] bg-[#0000008A] flex justify-center items-center">
-      <span className="flex gap-[2px] h-[26px] font-16-regular text-white">
-        <p>{reaction.emoji}</p>
-        <p>{reaction.count}</p>
-      </span>
+    <div className="rounded-[32px] w-[66px] px-[12px] py-[8px] bg-[#0000008A] flex justify-center items-center gap-[2px] text-white">
+      <span>{reaction.emoji}</span>
+      <span>{reaction.count > 99 ? "99+" : reaction.count}</span>
     </div>
   );
 };
