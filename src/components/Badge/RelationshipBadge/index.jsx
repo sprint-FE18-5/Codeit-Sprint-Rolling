653dc1f40029b7af423bdf5535fae05fd2eb21a0
sanginjeong <sanginjeong07@gmail.com>
2025-08-17 14:28:58 +0900

    ⚡️ Refactor: 관계 뱃지 시맨틱 태그 교체 + 스타일 수정

--- src/components/Badge/RelationshipBadge/index.jsx
+++ src/components/Badge/RelationshipBadge/index.jsx
@@ -4,8 +4,8 @@ const RelationshipBadge = ({ relationship }) => {
   const { bgColor, textColor } = COLOR_RELATIONSHIP[relationship];
 
   return (
-    <div className={`${bgColor} ${textColor} w-[41px] h-[20px] px-[8px] rounded-[4px]`}>
-      <p className="font-14-regular w-[25px] h-[20px]">{relationship}</p>
+    <div className={`${bgColor} ${textColor} w-[41px] h-[20px] px-[8px] rounded-[4px] flex`}>
+      <span className="font-14-regular w-[25px] h-[20px]">{relationship}</span>
     </div>
   );
 };
