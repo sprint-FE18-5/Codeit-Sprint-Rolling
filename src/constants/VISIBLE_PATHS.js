e799f09f08a50e52bb38c9f04cdacc473e76638c
sanginjeong <sanginjeong07@gmail.com>
2025-08-17 14:54:07 +0900

    🐛 Fix: header_service PATHS 수정

--- src/constants/VISIBLE_PATHS.js
+++ src/constants/VISIBLE_PATHS.js
@@ -1,4 +1,4 @@
 const VISIBLE_HEADER_BTN_PATHS = ["/", "/list"];
-const VISIBLE_HEADER_SERVICE_PATHS = ["/post", "/post/:id", "/post/:id/edit"];
+const VISIBLE_HEADER_SERVICE_PATHS = ["/post/:id", "/post/:id/edit"];
 
 export { VISIBLE_HEADER_BTN_PATHS, VISIBLE_HEADER_SERVICE_PATHS };
