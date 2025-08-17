ab199cefddc60263a462b4224aab951596a73e55
sanginjeong <sanginjeong07@gmail.com>
2025-08-17 17:12:10 +0900

    ⚡️ Refactor: COLOR_RELATIONSHIP 구조 수정

--- src/constants/COLOR_RELATIONSHIP.js
+++ src/constants/COLOR_RELATIONSHIP.js
@@ -1,20 +1,8 @@
 const COLOR_RELATIONSHIP = {
-  지인: {
-    bgColor: "bg-beige-100",
-    textColor: "text-beige-500",
-  },
-  동료: {
-    bgColor: "bg-purple-100",
-    textColor: "text-purple-500",
-  },
-  가족: {
-    bgColor: "bg-green-100",
-    textColor: "text-green-500",
-  },
-  친구: {
-    bgColor: "bg-blue-100",
-    textColor: "text-blue-500",
-  },
+  지인: "bg-beige-100 text-beige-500",
+  동료: "bg-purple-100 text-purple-500",
+  가족: "bg-green-100 text-green-500",
+  친구: "bg-blue-100 text-blue-500",
 };
 
 export default COLOR_RELATIONSHIP;
