cff53a619a3a736c33e518188cf5889a62210d22
YongHun <songyh11301@naver.com>
2025-08-18 17:47:16 +0900

    색상,폰트 통합

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import ColorPalette from "./components/ColorPalette";
 import Test from "./components/Test";
 const App = () => {
   return (
     <>
       <div className="p-2">
-        <ColorPalette />
         <Test />
       </div>
     </>
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,4 +1,4 @@
 const Test = () => {
-  return <div className="text-beige-500">테스트!</div>;
+  return <div className="text-beige-500 Font-18-Regular">테스트!</div>;
 };
 export default Test;
