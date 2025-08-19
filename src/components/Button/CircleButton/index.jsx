88ded84bc2baf8ca1a16c2ae12b0da25d586ab05
minyoung1101 <my11612@naver.com>
2025-08-20 02:57:40 +0900

    ✨ Feat: CircleButton width/height props 추가

--- src/components/Button/CircleButton/index.jsx
+++ src/components/Button/CircleButton/index.jsx
@@ -6,13 +6,15 @@ import CIRCLE_BUTTON from "../../../constants/button/BUTTON_CIRCLE.js";
  *
  * @param {Object} props
  * @param {string} [props.className] - 새로 추가할 className
+ * @param {number} [props.width] - 버튼 가로 길이
+ * @param {number} [props.height] - 버튼 높이
  * @param {React.ReactNode} [props.children] - 버튼 내부 콘텐츠 (아이콘, 텍스트 등)
  * @param {function} [props.onClick] - onClick 콜백 함수
  * @param {Object} [restProps] - 버튼에 전달되는 추가 속성(type, aria-*, value 등)
  *
  * @returns {JSX.Element}
  */
-const CircleButton = ({ className = "", children, onClick, ...restProps }) => {
+const CircleButton = ({ className = "", width = undefined, height = undefined, children, onClick, ...restProps }) => {
   const commonBtnStyle = COMMON_BUTTON;
   const { style: variantStyle, size: sizeStyle } = CIRCLE_BUTTON;
   const circleBtnStyle = [variantStyle.base, variantStyle.hover, variantStyle.active, variantStyle.focus, sizeStyle]
@@ -20,7 +22,12 @@ const CircleButton = ({ className = "", children, onClick, ...restProps }) => {
     .join(" ");
 
   return (
-    <button className={`${commonBtnStyle} ${circleBtnStyle} ${className}`} onClick={onClick} {...restProps}>
+    <button
+      className={`${commonBtnStyle} ${circleBtnStyle} ${className}`}
+      style={{ width, height }}
+      onClick={onClick}
+      {...restProps}
+    >
       {children}
     </button>
   );
