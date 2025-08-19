3400293715b8300664f563762890890310e41610
minyoung1101 <my11612@naver.com>
2025-08-20 04:15:27 +0900

    ✨ Feat: ToggleButton 추가 props 지원(토글/컬러/이미지 버튼)

--- src/components/Button/ToggleButton/index.jsx
+++ src/components/Button/ToggleButton/index.jsx
@@ -1,39 +1,34 @@
 import { useState } from "react";
-import Button from "../RegularButton";
-
-// 토글 영역 스타일
-const CONTAINER_STYLE = "w-[236px] md:w-[244px] h-[40px] bg-gray-100 rounded-[6px] flex";
-
-// 토글 상태(on/off) 스타일 - Button 컴포넌트의 "secondary", "outlined" 스타일 사용
-const STATE_SELECT = {
-  on: { variant: "secondary", className: "font-[700] border-[2px]" },
-  off: { variant: "outlined", className: "border-none" },
-};
-
-const getSelectStyle = isSelected => (isSelected ? STATE_SELECT.on : STATE_SELECT.off);
+import { TOGGLE_CONTAINER, getSelectStyle } from "../../../constants/button/BUTTON_TOGGLE";
+import RegularButton from "../RegularButton";
 
 /**
  * 토글 버튼 컴포넌트
  *
  * @param {Object} props
  * @param {string} [props.className] - 새로 추가할 className
+ * @param {Object} [props.toggleProps] - 토글 div에 전달할 props (id, aria-* 등)
+ * @param {Object} [props.colorBtnProps] - 컬러 버튼에 전달할 props
+ * @param {Object} [props.imageBtnProps] - 이미지 버튼에 전달할 props
+ *
  * @returns {JSX.Element} 컬러/이미지 중 하나를 선택하는 토글 버튼
  */
-const ToggleButton = ({ className = "" }) => {
+const ToggleButton = ({ className = "", toggleProps = {}, colorBtnProps = {}, imageBtnProps = {} }) => {
   const [selectedType, setSelectedType] = useState("color");
-  const colorBtnProps = getSelectStyle(selectedType === "color");
-  const imgBtnProps = getSelectStyle(selectedType === "image");
+
+  const colorBtnStyle = getSelectStyle(selectedType === "color");
+  const imageBtnStyle = getSelectStyle(selectedType === "image");
 
   const handleBtnClick = type => setSelectedType(type);
 
   return (
-    <div className={`toggleContainer ${CONTAINER_STYLE} ${className}`}>
-      <Button {...colorBtnProps} onClick={() => handleBtnClick("color")}>
+    <div className={`${TOGGLE_CONTAINER} ${className}`} {...toggleProps}>
+      <RegularButton {...colorBtnStyle} {...colorBtnProps} onClick={() => handleBtnClick("color")}>
         컬러
-      </Button>
-      <Button {...imgBtnProps} onClick={() => handleBtnClick("image")}>
+      </RegularButton>
+      <RegularButton {...imageBtnStyle} {...imageBtnProps} onClick={() => handleBtnClick("image")}>
         이미지
-      </Button>
+      </RegularButton>
     </div>
   );
 };
