f25fadb752efff8ff75cb6c9cf86e07fd266d46d
Yeonsu Lee <170384198+suuuuya@users.noreply.github.com>
2025-08-21 09:24:32 +0900

    ✨ Feat: Page 컴포넌트 추가 #78

---
+++ src/pages/Page.jsx
@@ -0,0 +1,11 @@
+import usePageTitle from "../hooks/usePageTitle";
+
+/**
+ * 페이지 타이틀 설정 컴포넌트
+ */
+const Page = ({ title = "", children }) => {
+  usePageTitle(title);
+  return <>{children}</>;
+};
+
+export default Page;
