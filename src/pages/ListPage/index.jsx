afb6fb5e7a47e677c85f04d82b0f1c42d8f238c6
minyoung1101 <my11612@naver.com>
2025-08-24 21:01:10 +0900

    ✨ Feat: 리스트페이지 스켈레톤 로더 구현

--- src/pages/ListPage/index.jsx
+++ src/pages/ListPage/index.jsx
@@ -7,6 +7,21 @@ import RegularButton from "../../components/Button/RegularButton";
 import useToast from "../../hooks/useToast";
 import COLOR_OPTION from "../../constants/COLOR_OPTION";
 
+const SkeletonCardList = () => {
+  return (
+    <div className="animate-pulse xl:w-[1160px] flex gap-[12px] md:gap-[20px]">
+      {Array.from({ length: 4 }).map((_, idx) => (
+        <div
+          key={idx}
+          className="bg-grayscale-300 rounded-2xl
+      w-[208px] h-[232px] min-w-[208px] min-h-[232px]
+      md:w-[275px] md:h-[260px] md:min-w-[208px] md:min-h-[232px]"
+        ></div>
+      ))}
+    </div>
+  );
+};
+
 const CardItems = cards => {
   return cards.map(item => (
     <CardList
@@ -25,6 +40,7 @@ const CardItems = cards => {
 
 const ListPage = () => {
   const { createToast } = useToast();
+  const [isLoading, setIsLoaing] = useState(true);
   const [recentList, setRecentList] = useState([]);
   const [popularList, setPopularList] = useState([]);
   const navigate = useNavigate();
@@ -40,6 +56,7 @@ const ListPage = () => {
         const popularRecipientsList = await getRecipientsList({ sort: "like" });
         setRecentList(recentRecipientsList);
         setPopularList(popularRecipientsList);
+        setIsLoaing(false);
       } catch (error) {
         console.log(error);
         createToast({ message: "데이터를 가져오는 데 실패했습니다.", type: "error", bottom: 40 });
@@ -55,11 +72,11 @@ const ListPage = () => {
     <div className="list-page pt-[65px] px-[24px]">
       <div className="list-popular lg:w-[1160px] mx-auto mt-[50px]">
         <h2 className="font-24-bold mb-[16px]">인기 롤링 페이퍼 🔥</h2>
-        <CardCarousel>{CardItems(popularListItem)}</CardCarousel>
+        <CardCarousel>{isLoading ? <SkeletonCardList /> : CardItems(popularListItem)}</CardCarousel>
       </div>
       <div className="list-recent  lg:w-[1160px] mx-auto mt-[50px]">
         <h2 className="font-24-bold mb-[16px]">최근에 만든 롤링 페이퍼 ⭐️</h2>
-        <CardCarousel>{CardItems(recentListItem)}</CardCarousel>
+        <CardCarousel>{isLoading ? <SkeletonCardList /> : CardItems(recentListItem)}</CardCarousel>
       </div>
       <RegularButton size={56} width="" className="w-full lg:w-[280px] mx-auto my-[64px]" onClick={handleBtnClick}>
         나도 만들어보기
